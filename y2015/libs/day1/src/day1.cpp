8dc7b5d7d1a542ce2efba1944009d5e140ee344a
Iñaki Pujol <ipujol10@gmail.com>
2023-08-31 12:58:12 +0200

    UPDATE: get the floor from a string

--- y2015/libs/day1/src/day1.cpp
+++ y2015/libs/day1/src/day1.cpp
@@ -2,5 +2,21 @@
 
 namespace d1 {
     int floor(const std::string &trajectory) {
+        int f = 0;
+
+        for (char dir : trajectory) {
+            switch (dir) {
+                case '(':
+                    f++;
+                    break;
+                case ')':
+                    f--;
+                    break;
+                default:
+                    throw "Character not valid";
+                    break;
+            }
+        }
+        return f;
     }
 }
--- y2015/libs/day1/test/test.cpp
+++ y2015/libs/day1/test/test.cpp
@@ -1,8 +1,29 @@
 #include <gtest/gtest.h>
 #include "D1_2015/day1.hpp"
 
-TEST(D1_2015Test, Single) {
+TEST(FloorTest, Single) {
     EXPECT_EQ(d1::floor("("), 1); // )
     EXPECT_EQ(d1::floor(")"), -1);
     EXPECT_EQ(d1::floor(""), 0);
+}
+
+TEST(FloorTest, F0) {
+    EXPECT_EQ(d1::floor("(())"), 0);
+    EXPECT_EQ(d1::floor("()()"), 0);
+}
+
+TEST(FloorTest, F3) {
+    EXPECT_EQ(d1::floor("((("), 3); // )))
+    EXPECT_EQ(d1::floor("(()(()("), 3); // )))
+    EXPECT_EQ(d1::floor("))((((("), 3); // )))
+}
+
+TEST(FloorTest, Fm1) {
+    EXPECT_EQ(d1::floor("())"), -1);
+    EXPECT_EQ(d1::floor("))("), -1);
+}
+
+TEST(FloorTest, Fm3) {
+    EXPECT_EQ(d1::floor(")))"), -3);
+    EXPECT_EQ(d1::floor(")())())"), -3);
 }
