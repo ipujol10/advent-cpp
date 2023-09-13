bee52c5f3f39a0460f8e815d55f0c6cd4ec25b18
Iñaki Pujol <ipujol10@gmail.com>
2023-09-13 20:55:03 +0200

    UPDATE: something is wrong

--- y2015/src/day9.cpp
+++ y2015/src/day9.cpp
@@ -2,6 +2,9 @@
 
 int main (int argc, char *argv[]) {
     std::string file_name = "../src/files/day9.txt";
+    d9::Distances d;
+    d.generateDistances(file_name);
+    std::cout << "The shortest distance is: " << d.getShortest() << std::endl;
     
     return 0;
 }
