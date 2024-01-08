c2b6b76d2f7e488ba7eaf5b71d3d8b280a6bcb7f
Iñaki Pujol <ipujol10@gmail.com>
2024-01-08 12:46:16 +0100

    UPDATE: implementation copied need to test

--- y2015/libs/day7/include/D7_2015/virtual.hpp
+++ y2015/libs/day7/include/D7_2015/virtual.hpp
@@ -45,6 +45,7 @@ protected:
   void setA() override;
 
 public:
+  OneInput(const std::string &a, const std::string &out);
   void setA(valType val) override;
   std::string getA() const override;
   virtual bool isOneEntry() const override;
@@ -59,8 +60,46 @@ protected:
   void setB() override;
 
 public:
+  TwoInputs(const std::string &a, const std::string &b, const std::string &out);
   void setB(valType val) override;
   std::string getB() const override;
   bool isOneEntry() const override;
+};
+
+class SetGate : protected OneInput {
+public:
+  SetGate(const std::string &a, const std::string &out);
+  std::optional<valType> execute() override;
+};
+
+class NotGate : protected OneInput {
+public:
+  NotGate(const std::string &a, const std::string &out);
+  std::optional<valType> execute() override;
+};
+
+class AndGate : protected TwoInputs {
+public:
+  AndGate(const std::string &a, const std::string &b, const std::string &out);
+  std::optional<valType> execute() override;
+};
+
+class OrGate : protected TwoInputs {
+public:
+  OrGate(const std::string &a, const std::string &b, const std::string &out);
+  std::optional<valType> execute() override;
+};
+
+class LeftShift : protected TwoInputs {
+public:
+  LeftShift(const std::string &a, const std::string &b, const std::string &out);
+  std::optional<valType> execute() override;
+};
+
+class RightShift : protected TwoInputs {
+public:
+  RightShift(const std::string &a, const std::string &b,
+             const std::string &out);
+  std::optional<valType> execute() override;
 };
 } // namespace d7v
--- y2015/libs/day7/src/virtual.cpp
+++ y2015/libs/day7/src/virtual.cpp
@@ -1,9 +1,20 @@
 #include "virtual.hpp"
 
 namespace d7v {
-  std::string Gate::getOut() const {
-    return out;
-  }
+std::string Gate::getOut() const { return out; }
+
+bool Gate::operator>(const Gate &left) const {
+  return this->priority > left.priority;
+}
+
+bool Gate::operator>=(const Gate &left) const {
+  return this->priority >= left.priority;
+}
+
+OneInput::OneInput(const std::string &a, const std::string &out) : a_name(a) {
+  this->out = out;
+  setA();
+}
 
 void OneInput::setA() {
   try {
@@ -19,19 +30,21 @@ void OneInput::setA(valType val) {
   a = val;
 }
 
-std::string OneInput::getA() const {
-  return a_name;
-}
+std::string OneInput::getA() const { return a_name; }
 
-bool OneInput::isOneEntry() const {
-  return true;
+bool OneInput::isOneEntry() const { return true; }
+
+TwoInputs::TwoInputs(const std::string &a, const std::string &b,
+                     const std::string &out)
+    : OneInput(a, out), b_name(b) {
+  setB();
 }
 
 void TwoInputs::setB() {
   try {
     this->b = (valType)std::stoi(this->a_name);
     this->a_set = true;
-  } catch(...) {
+  } catch (...) {
     this->a_set = false;
   }
 }
@@ -41,11 +54,95 @@ void TwoInputs::setB(valType val) {
   b = val;
 }
 
-std::string TwoInputs::getB() const {
-  return b_name;
+std::string TwoInputs::getB() const { return b_name; }
+
+bool TwoInputs::isOneEntry() const { return false; }
+
+SetGate::SetGate(const std::string &a, const std::string &out)
+    : OneInput(a, out) {
+  priority = 0;
+  type = gateType::SetGate;
 }
 
-bool TwoInputs::isOneEntry() const {
-  return false;
+std::optional<valType> SetGate::execute() {
+  priority++;
+  if (!a_set) {
+    return {};
+  }
+  return a;
 }
+
+NotGate::NotGate(const std::string &a, const std::string &out)
+    : OneInput(a, out) {
+  priority = 5;
+  type = gateType::NotGate;
 }
+
+std::optional<valType> NotGate::execute() {
+  priority++;
+  if (!a_set) {
+    return {};
+  }
+  return ~a;
+}
+
+AndGate::AndGate(const std::string &a, const std::string &b,
+                 const std::string &out)
+    : TwoInputs(a, b, out) {
+  priority = 15;
+  type = gateType::AndGate;
+}
+
+std::optional<valType> AndGate::execute() {
+  priority++;
+  if (!(a_set && b_set)) {
+    return {};
+  }
+  return a & b;
+}
+
+OrGate::OrGate(const std::string &a, const std::string &b,
+               const std::string &out)
+    : TwoInputs(a, b, out) {
+  priority = 15;
+  type = gateType::OrGate;
+}
+
+std::optional<valType> OrGate::execute() {
+  priority++;
+  if (!(a_set && b_set)) {
+    return {};
+  }
+  return a | b;
+}
+
+LeftShift::LeftShift(const std::string &a, const std::string &b,
+                     const std::string &out)
+    : TwoInputs(a, b, out) {
+  priority = 10;
+  type = gateType::LeftShift;
+}
+
+std::optional<valType> LeftShift::execute() {
+  priority++;
+  if (!(a_set && b_set)) {
+    return {};
+  }
+  return a << b;
+}
+
+RightShift::RightShift(const std::string &a, const std::string &b,
+                       const std::string &out)
+    : TwoInputs(a, b, out) {
+  priority = 10;
+  type = gateType::RightShift;
+}
+
+std::optional<valType> RightShift::execute() {
+  priority++;
+  if (!(a_set && b_set)) {
+    return {};
+  }
+  return a >> b;
+}
+} // namespace d7v
